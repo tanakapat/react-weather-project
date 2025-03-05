8fa1fbef02a6d7e92a2a6e11517aa8906f845211
tanakapat <patricia.tanaka.3@hotmail.com>
2025-03-05 00:40:17 -0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Pat Tanaka and it is open sourced on
-      {" "}<a href='https://github.com/tanakapat/react-weather-project' target='blank'>Github</a>
-      </footer>
+      <div className='container'>
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Pat Tanaka and it is open sourced on
+        {" "}<a href='https://github.com/tanakapat/react-weather-project' target='blank'>Github</a>
+        </footer>
+      </div>
     </div>
   );
 }
