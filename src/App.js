66f0c15a6272ba2aa18bba445d6b4cdbcd5d365c
tanakapat <patricia.tanaka.3@hotmail.com>
2025-03-05 00:20:48 -0300

    Added a footer and Github repository URL

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ export default function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+        This project was coded by Pat Tanaka and it is open sourced on
+      {" "}<a href='https://github.com/tanakapat/react-weather-project' target='blank'>Github</a>
+      </footer>
     </div>
   );
 }
